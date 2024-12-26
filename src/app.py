460befda410255f49e6e099067bfd5118d0356aa
Prateekkumawat <prateekkumawat20@gmail.com>
2024-12-26 18:32:34 +0530

    add

--- src/app.py
+++ src/app.py
@@ -5,11 +5,19 @@ from redis import Redis
 app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
 
+# @app.route('/')
+# def hello():
+#     redis.incr('hits')
+#     return 'This Compose/Flask demo has been viewed by me on  %s time(s).' % redis.get('hits')
+
 @app.route('/')
 def hello():
-    redis.incr('hits')
-    return 'This Compose/Flask demo has been viewed by me on  %s time(s).' % redis.get('hits')
-
+    try:
+        redis.incr('hits')
+        hits = redis.get('hits')
+        return f"This Compose/Flask demo has been viewed by me {hits} time(s)."
+    except RedisError as e:
+        return f"Error connecting to Redis: {str(e)}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
