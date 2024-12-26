644979f5e51699d07ec0530d21f8dc34aa935099
Prateekkumawat <prateekkumawat20@gmail.com>
2024-12-26 18:21:40 +0530

    code

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ redis = Redis(host='redis', port=6379)
 @app.route('/')
 def hello():
     redis.incr('hits')
-    return 'This Compose/Flask demo has been viewed %s time(s).' % redis.get('hits')
+    return 'This Compose/Flask demo has been viewed by me on  %s time(s).' % redis.get('hits')
 
 
 if __name__ == "__main__":
